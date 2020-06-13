680e20c26661c8329e719fc47ec72f06d59236d1
unknown <ibnushevayanto@gmail.com>
2020-06-13 08:32:05 +0700

    Progress

---
+++ basics-01-starting-project/assets/scripts/assignment1.js
@@ -0,0 +1,14 @@
+let result;
+const userInput = 19;
+
+result = userInput + 18;
+console.log(`First Result Is ${result}`)
+result = result - userInput;
+console.log(`Second Result Is ${result} `)
+result = result / userInput;
+console.log(`Third Result Is ${result} `)
+result = result * userInput;
+console.log(`Fourth Result Is ${result} `)
+
+alert(`The User Input Is ${userInput}`)
+alert(`The Result Is ${result}`)
